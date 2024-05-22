82da72e385f1e19b15cb1dd693a8f59ca2d9d5ac
Hung Nguyen <nthungdev@gmail.com>
2024-05-22 00:50:19 -0400

    demo

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,42 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (cfg) => {
+    cfg.module.rules.push(
+      {
+        test: /\.md$/,
+        loader: 'frontmatter-markdown-loader',
+        options: { mode: ['react-component'] }
+      }
+    )
+    return cfg
+  },
+  redirects: async () => {
+    return [
+      {
+        source: '/admin',
+        destination: '/admin/index.html',
+        permanent: false,
+      },
+      // {
+      //   source: '/',
+      //   has: [{ type: 'query', key: '#invite_token' }],
+      //   destination: '/admin/index.html',
+      //   permanent: true,
+      // },
+      // {
+      //   source: '/#',
+      //   has: [{ type: 'query', key: 'confirmation_token' }],
+      //   destination: '/admin/index.html',
+      //   permanent: true,
+      // },
+      // {
+      //   source: '/#',
+      //   has: [{ type: 'query', key: 'invite_token' }],
+      //   destination: '/admin/index.html',
+      //   permanent: true,
+      // },
+    ]
+  },
+};
 
 export default nextConfig;
