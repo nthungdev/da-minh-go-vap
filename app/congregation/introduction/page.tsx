384b69d08512f07d089cc0ba5e0cbe18d67dabcb
Hung Nguyen <nthungdev@gmail.com>
2024-06-11 17:58:05 -0400

    progress

--- app/congregation/introduction/page.tsx
+++ app/congregation/introduction/page.tsx
@@ -1,19 +1,53 @@
 import {
-  // react as Congregration,
   attributes,
-  body
+  body,
 } from '@/content/pages/congregation/introduction.md'
 import AppMarkdown from '@/components/app-markdown'
 
-export default function CongregationIntroduction() {
+const AboutSection = ({ aboutUs }:  Readonly<{
+  aboutUs: AboutUsSection;
+}>) => {
+  return (
+    <section>
+      <h3 className="text-xl">{aboutUs.title}</h3>
+      <AppMarkdown>{aboutUs.body}</AppMarkdown>
+      {aboutUs.tabs.map(({ title, body }) => (
+        <div key={title}>
+          <h4>{title}</h4>
+          <AppMarkdown>{body}</AppMarkdown>
+        </div>
+      ))}
+      <h4>Video giới thiệu</h4>
+      {/* TODO */}
+    </section>
+  )
+}
 
-  const {
-    submenus: { introduction = '' } = {},
-  } = attributes
+export default function CongregationIntroduction() {
+  const { aboutUs, mission, whereabouts } = attributes as CongregationAbout
 
   return (
-    <main>
-      <AppMarkdown>{body}</AppMarkdown>
+    <main className="space-y-4">
+      {/* <AboutSection aboutUs={aboutUs} /> */}
+
+      <section>
+        {/* TODO format as tabs */}
+        {aboutUsTabs.map(({ title, content }) => (
+          <div key={title}>
+            <h3 className="text-xl">{title}</h3>
+            <AppMarkdown>{content}</AppMarkdown>
+          </div>
+        ))}
+      </section>
+
+      <section>
+        <h3 className="text-xl">Video giới thiệu</h3>
+        {/* TODO */}
+      </section>
+
+      <section>
+        <h3 className="text-xl">Nhiệm vụ</h3>
+      </section>
     </main>
   )
 }
--- definitions/index.ts
+++ definitions/index.ts
@@ -1,4 +1,38 @@
 type Home = {
-  title: string,
-  cats: string,
+  title: string
+  cats: string
+}
+
+type AboutUsSection = {
+  banner: string
+  title: string
+  body: string
+  tabs: {
+    title: string
+    body: string
+  }[]
+  videos: {
+    title: string
+    videos: {
+      title: string
+      youtubeUrl: string
+    }[]
+  }
+}
+
+type MissionSection = AboutUsSection
+
+type WhereaboutsSection = {
+  banner: string
+  title: string
+  countries: {
+    name: string
+    body: string
+  }[]
+}
+
+type CongregationAbout = {
+  aboutUs: AboutUsSection
+  mission: MissionSection
+  whereabouts: WhereaboutsSection
 }
