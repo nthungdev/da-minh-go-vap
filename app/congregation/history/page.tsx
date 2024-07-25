000555fd2ed8b3855846ce938d1b93f832fd30b7
Hung Nguyen <31390562+nthungdev@users.noreply.github.com>
2024-07-24 22:56:03 -0400

    congregation history (#8)

--- app/congregation/history/page.tsx
+++ app/congregation/history/page.tsx
@@ -1,8 +1,30 @@
+import AppPage from "@/components/app-page";
+import { attributes } from '@/content/pages/congregation/history.md'
+
 /**
  * History
  */
 export default function CongregationHistory() {
+  const { developmentHistory, communityHistory } = attributes as CongregationHistory
+
   return (
-    <main>Lich su</main>
+    <AppPage className="space-y-8">
+      <section className="space-y-4">
+        <h2 className="text-2xl">{developmentHistory.title}</h2>
+
+        <ol className="space-y-4">
+          {developmentHistory.timeline.map(({ title, body }, index) => (
+            <li key={index}>
+              <h3 className="text-lg">{title}</h3>
+              <p>{body}</p>
+            </li>
+          ))}
+        </ol>
+      </section>
+
+      <section>
+        <h2 className="text-2xl">{communityHistory.title}</h2>
+      </section>
+    </AppPage>
   )
 }
---
+++ content/pages/congregation/history.md
@@ -0,0 +1,17 @@
+---
+developmentHistory:
+  title: Lịch sử phát triển hội dòng
+  body: Điền nội dung
+  timeline:
+    - title: "PHẦN 1: THỜI KÌ SƠ KHAI (1918 - 1956)"
+      body: Nội dung
+    - title: "PHẦN II: THỜI KỲ CẢI TỔ VÀ PHÁT TRIỂN (1956 - 1978)"
+      body: Nội dung
+    - title: "PHẦN III: THỜI KỲ CHÍNH THỨC LÀ HỘI DÒNG ĐA MINH LẠNG SƠN, THÁNH HIỆU
+        ĐỨC MẸ MẦN CÔI GIÁO PHẬN TP. HỒ CHÍ MÌNH (1978 - 2013)"
+      body: Nội dung
+  videos:
+    title: Video về lịch sử (các giai đoạn phát triển hội dòng)
+communityHistory:
+  title: Lịch sử các cộng đoàn
+---
