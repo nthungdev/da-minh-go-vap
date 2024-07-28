7b64e276b1134b1e0ec27d000b51bda9a9dcda3c
Hung Nguyen <nthungdev@gmail.com>
2024-07-27 23:14:31 -0400

    add Post Page

---
+++ utils/posts.ts
@@ -0,0 +1,23 @@
+import path from 'path'
+import fs from 'fs'
+import matter from 'gray-matter'
+
+const postsDirectory = path.join(process.cwd(), 'content/posts')
+
+export const getPostBySlug = (slug: string) => {
+  const filePath = path.join(postsDirectory, slug + '.md')
+  const fileContents = fs.readFileSync(filePath, 'utf8')
+  const { content, data } = matter(fileContents)
+  return {
+    slug,
+    body: content,
+    ...data,
+  } as PostParams
+}
+
+export const getAllPostSlugs = () => {
+  const fileNames = fs.readdirSync(postsDirectory)
+  return fileNames.map(fileName => {
+    return fileName.replace(/\.md$/, '')
+  })
+}
