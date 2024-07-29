bc7bff830dd0930d0588536b46f52a27e32b5813
Hung Nguyen <nthungdev@gmail.com>
2024-07-28 20:29:26 -0400

    update - AppPostGrid - Hoi Dong page

--- app/congregation/page.tsx
+++ app/congregation/page.tsx
@@ -1,9 +1,42 @@
-import { attributes } from '@/content/pages/congregration.md'
+import AppPage from '@/components/app-page'
+import { getPostsByCategories } from '@/utils/posts'
+import AppPostGrid from '@/components/app-post-grid'
 
 export default function History() {
-  console.log({ attributes })
+  const introductionPosts = getPostsByCategories(['Giới thiệu'], { limit: 4 })
+  const historyPosts = getPostsByCategories(['Lịch sử'], { limit: 4 })
+  const establishmentPosts = getPostsByCategories(['Sắc lập dòng'], { limit: 4 })
+  const authoritiesPosts = getPostsByCategories(['Các đấng bản quyền'], { limit: 4 })
+
+  const data = [
+    {
+      title: 'Giới thiệu',
+      posts: introductionPosts
+    },
+    {
+      title: 'Lịch sử',
+      posts: historyPosts
+    },
+    {
+      title: 'Sắc lập dòng',
+      posts: establishmentPosts
+    },
+    {
+      title: 'Các đấng bản quyền',
+      posts: authoritiesPosts,
+    }
+  ].filter(({ posts }) => posts.length > 0)
 
   return (
-    <main>Lich Su Page</main>
+    <AppPage>
+      <ul className='space-y-8'>
+        {data.map(({ title, posts }, index) => (
+          <li key={index}>
+            <h2 className='uppercase mb-2 text-2xl'>{title}</h2>
+            <AppPostGrid posts={posts} />
+          </li>
+        ))}
+      </ul>
+    </AppPage>
   )
 }
---
+++ content/post-categories/lịch-sử-1.md
@@ -0,0 +1,3 @@
+---
+title: Các đấng bản quyền
+---
