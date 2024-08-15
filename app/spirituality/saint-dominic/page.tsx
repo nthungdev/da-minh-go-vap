0ab9d0c5c89f2df7e4d617dc308fd90e595462d6
Hung Nguyen <nthungdev@gmail.com>
2024-08-14 23:05:17 -0400

    placeholder Spirituality Saint Dominic

---
+++ app/spirituality/saint-dominic/page.tsx
@@ -0,0 +1,11 @@
+import AppPage from '@/components/app-page'
+import { attributes } from '@/content/pages/spirituality/charism.md'
+
+export default function SpiritualitySaintDominic() {
+
+  return (
+    <AppPage>
+      Saint Dominic
+    </AppPage>
+  )
+}
