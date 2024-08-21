7a5c8d3812990ce85dcb9453ce86ca891205b913
Hung Nguyen <nthungdev@gmail.com>
2024-08-21 01:15:09 -0400

    update not found page style

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,7 @@ export default function NotFound() {
   return (
     <AppPage className='flex-1 flex flex-col justify-center items-center'>
       <h1 className='text-4xl'>Không Tìm Thấy</h1>
-      <p>Trang này không tồn tại</p>
+      <p className='mt-4'>Trang này không tồn tại</p>
       <Link href="/" className='mt-10'>Về Trang Chủ</Link>
     </AppPage>
   )
