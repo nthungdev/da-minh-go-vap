3b27ddc75f8f5b48ef9cc813efd98c563fd43ecf
Hung Nguyen <nthungdev@gmail.com>
2024-09-04 01:13:04 -0400

    add bg color to accordion

--- components/app-accordion.tsx
+++ components/app-accordion.tsx
@@ -18,13 +18,15 @@ export default function AppAccordion({ data }: AppAccordionProps) {
       {data.map(({ title, body }, index) => (
         <div
           key={index}
-          className={`hs-accordion ${index === activeIndex ? 'active' : ''}`}
+          className={`hs-accordion rounded-lg bg-gray-200 ${
+            index === activeIndex ? 'active' : ''
+          }`}
           id={`hs-basic-with-arrow-heading-${index}`}
         >
           <button
-            className={
-              `hs-accordion-toggle hs-accordion-active:text-primary-600 py-3 inline-flex items-center gap-x-3 w-full font-semibold text-start text-gray-800 hover:text-gray-500 focus:outline-none focus:text-gray-500 rounded-lg disabled:opacity-50 disabled:pointer-events-none ${index === activeIndex ? 'dhs-accordion-toggle' : ''}`
-            }
+            className={`hs-accordion-toggle hs-accordion-active:text-primary-600 hs-accordion-active:hover:text-gray-50 bg-primary-200 py-3 px-2 inline-flex items-center gap-x-3 w-full font-semibold text-start text-gray-800 hover:bg-primary hover:text-gray-50 focus:outline-none rounded-lg disabled:opacity-50 disabled:pointer-events-none ${
+              index === activeIndex ? 'dhs-accordion-toggle' : ''
+            }`}
             aria-expanded={index === activeIndex}
             aria-controls={
               index === activeIndex
@@ -65,13 +67,16 @@ export default function AppAccordion({ data }: AppAccordionProps) {
           </button>
           <div
             id={`hs-basic-with-arrow-collapse-${index}`}
-            className={`hs-accordion-content w-full overflow-hidden transition-[height] duration-300 ${index === activeIndex ? '' : 'hidden'}`}
+            className={`hs-accordion-content w-full overflow-hidden transition-[height] duration-300 ${
+              index === activeIndex ? '' : 'hidden'
+            }`}
             role="region"
             aria-labelledby={`hs-basic-with-arrow-heading-${index}`}
           >
-            <AppMarkdown>{body}</AppMarkdown>
+            <div className="px-4 py-2">
+              <AppMarkdown>{body}</AppMarkdown>
+            </div>
           </div>
-
         </div>
       ))}
     </div>
