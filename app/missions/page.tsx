ce8771fb388b4636fe9584020a3ee9fe724ece24
Hung Nguyen <31390562+nthungdev@users.noreply.github.com>
2024-09-09 02:29:09 -0400

    Section separators (#84)
    * separators for home page
    
    * separator for missions
    
    * background color for AppPostTabGrid
    
    * update Congregation page
    
    * refactor AppSeparator

--- app/missions/page.tsx
+++ app/missions/page.tsx
@@ -1,28 +1,29 @@
 import AppPage from '@/components/app-page'
 import AppPostGrid from '@/components/app-post-grid'
 import AppPostTabGrid from '@/components/app-post-tab-grid'
+import AppSeparator from '@/components/app-separator'
 import { attributes } from '@/content/pages/missions/index.md'
 import { getPostsByHiddenTags } from '@/utils/posts'
 
 export default function PageMissions() {
   const { title, sections } = attributes as PageMissions
 
-  const sectionData = sections.map(section => {
+  const sectionData = sections.map((section) => {
     if (section.type === 'postSection') {
       return {
         ...section,
-        posts: getPostsByHiddenTags(section.hiddenTags,
-          { limit: section.limit || undefined }
-        ),
+        posts: getPostsByHiddenTags(section.hiddenTags, {
+          limit: section.limit || undefined,
+        }),
       }
     } else {
       return {
         ...section,
-        categories: section.categories.map(category => ({
+        categories: section.categories.map((category) => ({
           ...category,
-          posts: getPostsByHiddenTags(category.hiddenTags,
-            { limit: section.limit || undefined }
-          ),
+          posts: getPostsByHiddenTags(category.hiddenTags, {
+            limit: section.limit || undefined,
+          }),
         })),
       }
     }
@@ -30,20 +31,21 @@ export default function PageMissions() {
 
   return (
     <AppPage>
-      <h1 className='sr-only'>{title}</h1>
+      <h1 className="sr-only">{title}</h1>
 
-      <div className='space-y-12'>
-        {sectionData.map((section, index) =>
-        (
-          <div key={index} className='space-y-4'>
-            <h1 className="uppercase text-2xl bg-primary-800 p-2.5 text-center text-gray-50 rounded-lg">{section.title}</h1>
-            {
-              section.type === 'postSection' ? (
-                <AppPostGrid posts={section.posts!} />
-              ) : (
-                <AppPostTabGrid id={`grid-${index}`} subCategories={section.categories!} />
-              )
-            }
+      <div className="space-y-4">
+        {sectionData.map((section, index) => (
+          <div key={index} className="space-y-4">
+            <h1 className="uppercase text-2xl text-center">{section.title}</h1>
+            {section.type === 'postSection' ? (
+              <AppPostGrid posts={section.posts!} />
+            ) : (
+              <AppPostTabGrid
+                id={`grid-${index}`}
+                subCategories={section.categories!}
+              />
+            )}
+            {index !== sectionData.length - 1 && <AppSeparator />}
           </div>
         ))}
       </div>
