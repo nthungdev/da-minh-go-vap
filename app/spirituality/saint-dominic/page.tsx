4d277cfdd3273709ea4b57e2d56361a195ded9a0
Hung Nguyen <nthungdev@gmail.com>
2024-09-08 20:35:39 -0400

    rename post title

--- app/spirituality/saint-dominic/page.tsx
+++ app/spirituality/saint-dominic/page.tsx
@@ -5,24 +5,24 @@ import { attributes } from '@/content/pages/spirituality/saint-dominic.md'
 import { getPostsByHiddenTags } from '@/utils/posts'
 
 export default function SpiritualitySaintDominic() {
-  const { title, banners, hiddenTags } = attributes as PageSpiritualitySaintDominic
+  const { title, banners, hiddenTags } =
+    attributes as PageSpiritualitySaintDominic
   const posts = getPostsByHiddenTags(hiddenTags)
 
   return (
     <div>
       <AppBanners banners={banners} />
 
-      <AppPage className='space-y-12'>
-        <h1 className='sr-only'>{title}</h1>
+      <AppPage className="space-y-12">
+        <h1 className="sr-only">{title}</h1>
 
         {posts.length > 0 && (
           <section>
-            <h2 className='sr-only'>Các bài viết</h2>
+            <h2 className="sr-only">Các bài viết</h2>
             <AppPostGrid posts={posts} />
           </section>
         )}
       </AppPage>
-
     </div>
   )
 }
