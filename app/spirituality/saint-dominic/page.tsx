3ab4543dcb445a31e9ebb059b10de8b611e425ad
Hung Nguyen <nthungdev@gmail.com>
2024-09-08 20:31:43 -0400

    fix TheBanner

--- app/spirituality/saint-dominic/page.tsx
+++ app/spirituality/saint-dominic/page.tsx
@@ -1,7 +1,6 @@
+import AppBanners from '@/components/app-banners'
 import AppPage from '@/components/app-page'
 import AppPostGrid from '@/components/app-post-grid'
-import TheBanner from '@/components/the-banner'
-import TheDominicQuote from '@/components/the-dominic-quote'
 import { attributes } from '@/content/pages/spirituality/saint-dominic.md'
 import { getPostsByHiddenTags } from '@/utils/posts'
 
@@ -11,7 +10,7 @@ export default function SpiritualitySaintDominic() {
 
   return (
     <div>
-      <TheBanner banners={banners} />
+      <AppBanners banners={banners} />
 
       <AppPage className='space-y-12'>
         <h1 className='sr-only'>{title}</h1>
