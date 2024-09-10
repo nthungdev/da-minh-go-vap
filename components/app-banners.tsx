59c4660a6dc1627269a2f9643ace6ffed3d304df
Hung Nguyen <nthungdev@gmail.com>
2024-09-10 15:58:07 -0400

    handle carousel with 1 child

--- components/app-banners.tsx
+++ components/app-banners.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import Image from 'next/image'
-import { useEffect, useState } from 'react'
 import AppCarousel from './app-carousel'
 
 interface AppBannersProps {
@@ -13,28 +12,11 @@ interface AppBannersProps {
 
 export default function AppBanners(props: AppBannersProps) {
   const { banners } = props
-  // const [bannerIndex, setBannerIndex] = useState(0)
-
-  // const banner = (banners || [])[bannerIndex]
-  // support more video extensions
-  // const isVideo = banner?.url?.includes('.mp4')
-  // const bannerUrl = banner?.url || ''
 
   const checkVideo = (url: string) => {
     const videoExtensions = ['.mp4', '.webm', '.ogg']
     return videoExtensions.some((ext) => url.includes(ext))
   }
-
-  // useEffect(() => {
-  //   if (banners.length <= 1) return
-
-  //   const interval = setInterval(() => {
-  //     const nextIndex = (bannerIndex + 1) % banners.length
-  //     setBannerIndex(nextIndex)
-  //   }, 3000)
-
-  //   return () => clearInterval(interval)
-  // }, [banners, bannerIndex])
 
   return (
     <div className="aspect-[4]">
@@ -43,7 +25,7 @@ export default function AppBanners(props: AppBannersProps) {
           <div key={index} className="w-full h-full">
             {checkVideo(banner.url) ? (
               <video
-                className="object-cover"
+                className="w-full h-full object-cover"
                 src={banner.url}
                 autoPlay
                 loop
@@ -51,12 +33,11 @@ export default function AppBanners(props: AppBannersProps) {
               />
             ) : (
               <Image
-                className="object-cover"
+                className="w-full h-full object-cover"
                 src={banner.url}
                 alt={banner.alt || ''}
                 sizes="100%"
-                width={256}
-                height={144}
+                fill
               />
             )}
           </div>
--- components/app-carousel.tsx
+++ components/app-carousel.tsx
@@ -1,4 +1,4 @@
-import { Children } from 'react'
+import { Children, Fragment } from 'react'
 
 interface AppCarouselProps {
   children: React.ReactNode
@@ -10,6 +10,9 @@ export default function AppCarousel(props: AppCarouselProps) {
   const hsCarouselData = {
     loadingClasses: 'opacity-0',
   }
+
+  const childrenCount = Children.count(children)
+  const hasMultipleChildren = childrenCount > 1
 
   return (
     <div data-hs-carousel={JSON.stringify(hsCarouselData)} className="relative">
@@ -23,59 +26,63 @@ export default function AppCarousel(props: AppCarouselProps) {
         </div>
       </div>
 
-      <button
-        type="button"
-        className="hs-carousel-prev hs-carousel:disabled:opacity-50 disabled:pointer-events-none absolute inset-y-0 start-0 inline-flex justify-center items-center w-[46px] h-full text-gray-800 hover:bg-gray-800/10 focus:outline-none focus:bg-gray-800/10 rounded-s-lg"
-      >
-        <span className="text-2xl" aria-hidden="true">
-          <svg
-            className="shrink-0 size-5"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
+      {hasMultipleChildren && (
+        <Fragment>
+          <button
+            type="button"
+            className="hs-carousel-prev hs-carousel:disabled:opacity-50 disabled:pointer-events-none absolute inset-y-0 start-0 inline-flex justify-center items-center w-[46px] h-full text-gray-800 hover:bg-gray-800/10 focus:outline-none focus:bg-gray-800/10 rounded-s-lg"
           >
-            <path d="m15 18-6-6 6-6"></path>
-          </svg>
-        </span>
-        <span className="sr-only">Previous</span>
-      </button>
-      <button
-        type="button"
-        className="hs-carousel-next hs-carousel:disabled:opacity-50 disabled:pointer-events-none absolute inset-y-0 end-0 inline-flex justify-center items-center w-[46px] h-full text-gray-800 hover:bg-gray-800/10 focus:outline-none focus:bg-gray-800/10 rounded-e-lg"
-      >
-        <span className="sr-only">Next</span>
-        <span className="text-2xl" aria-hidden="true">
-          <svg
-            className="shrink-0 size-5"
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
+            <span className="text-2xl" aria-hidden="true">
+              <svg
+                className="shrink-0 size-5"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="m15 18-6-6 6-6"></path>
+              </svg>
+            </span>
+            <span className="sr-only">Previous</span>
+          </button>
+          <button
+            type="button"
+            className="hs-carousel-next hs-carousel:disabled:opacity-50 disabled:pointer-events-none absolute inset-y-0 end-0 inline-flex justify-center items-center w-[46px] h-full text-gray-800 hover:bg-gray-800/10 focus:outline-none focus:bg-gray-800/10 rounded-e-lg"
           >
-            <path d="m9 18 6-6-6-6"></path>
-          </svg>
-        </span>
-      </button>
+            <span className="sr-only">Next</span>
+            <span className="text-2xl" aria-hidden="true">
+              <svg
+                className="shrink-0 size-5"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="m9 18 6-6-6-6"></path>
+              </svg>
+            </span>
+          </button>
 
-      <div className="hs-carousel-pagination flex justify-center absolute bottom-3 start-0 end-0 space-x-2">
-        {Children.map(children, (_, index) => (
-          <span
-            key={index}
-            className="hs-carousel-active:bg-primary-700 hs-carousel-active:border-primary-700 size-3 border border-gray-400 rounded-full cursor-pointer dark:border-neutral-600 dark:hs-carousel-active:bg-primary-500 dark:hs-carousel-active:border-primary-500"
-          ></span>
-        ))}
-      </div>
+          <div className="hs-carousel-pagination flex justify-center absolute bottom-3 start-0 end-0 space-x-2">
+            {Children.map(children, (_, index) => (
+              <span
+                key={index}
+                className="hs-carousel-active:bg-primary-700 hs-carousel-active:border-primary-700 size-3 border border-gray-400 rounded-full cursor-pointer dark:border-neutral-600 dark:hs-carousel-active:bg-primary-500 dark:hs-carousel-active:border-primary-500"
+              ></span>
+            ))}
+          </div>
+        </Fragment>
+      )}
     </div>
   )
 }
