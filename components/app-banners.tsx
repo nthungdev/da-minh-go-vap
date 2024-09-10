3bfb8e762bfc12cb699cd46c6188e9e20f20d5e2
Hung Nguyen <nthungdev@gmail.com>
2024-09-10 15:54:03 -0400

    update AppBanners

--- components/app-banners.tsx
+++ components/app-banners.tsx
@@ -2,6 +2,7 @@
 
 import Image from 'next/image'
 import { useEffect, useState } from 'react'
+import AppCarousel from './app-carousel'
 
 interface AppBannersProps {
   banners: {
@@ -12,41 +13,55 @@ interface AppBannersProps {
 
 export default function AppBanners(props: AppBannersProps) {
   const { banners } = props
-  const [bannerIndex, setBannerIndex] = useState(0)
+  // const [bannerIndex, setBannerIndex] = useState(0)
 
-  const banner = (banners || [])[bannerIndex]
-  const isVideo = banner?.url?.includes('.mp4')
-  const bannerUrl = banner?.url || ''
+  // const banner = (banners || [])[bannerIndex]
+  // support more video extensions
+  // const isVideo = banner?.url?.includes('.mp4')
+  // const bannerUrl = banner?.url || ''
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      const nextIndex = (bannerIndex + 1) % banners.length
-      setBannerIndex(nextIndex)
-    }, 5000)
+  const checkVideo = (url: string) => {
+    const videoExtensions = ['.mp4', '.webm', '.ogg']
+    return videoExtensions.some((ext) => url.includes(ext))
+  }
 
-    return () => clearInterval(interval)
-  }, [banners, bannerIndex])
+  // useEffect(() => {
+  //   if (banners.length <= 1) return
+
+  //   const interval = setInterval(() => {
+  //     const nextIndex = (bannerIndex + 1) % banners.length
+  //     setBannerIndex(nextIndex)
+  //   }, 3000)
+
+  //   return () => clearInterval(interval)
+  // }, [banners, bannerIndex])
 
   return (
     <div className="aspect-[4]">
-      {isVideo ? (
-        <video
-          className="w-full h-full object-cover"
-          src={bannerUrl}
-          autoPlay
-          loop
-          muted
-        />
-      ) : (
-        <Image
-          className="w-full h-full object-cover"
-          src={bannerUrl}
-          alt={banner?.alt || ''}
-          sizes="100%"
-          width={256}
-          height={144}
-        />
-      )}
+      <AppCarousel>
+        {banners.map((banner, index) => (
+          <div key={index} className="w-full h-full">
+            {checkVideo(banner.url) ? (
+              <video
+                className="object-cover"
+                src={banner.url}
+                autoPlay
+                loop
+                muted
+              />
+            ) : (
+              <Image
+                className="object-cover"
+                src={banner.url}
+                alt={banner.alt || ''}
+                sizes="100%"
+                width={256}
+                height={144}
+              />
+            )}
+          </div>
+        ))}
+      </AppCarousel>
     </div>
   )
 }
