982baff714adfd9d755ad6de48907a4135b91c32
Hung Nguyen <nthungdev@gmail.com>
2024-09-23 02:48:26 -0400

    introduce AppMultiplePostGrids

--- components/app-grouped-posts-page.tsx
+++ components/app-grouped-posts-page.tsx
@@ -1,7 +1,5 @@
+import AppMultiplePostGrids from './app-multiple-post-grids'
 import AppPage from './app-page'
-import AppPostGrid from './app-post-grid'
-import AppSeparator from './app-separator'
-import { fetchPostsByHiddenTags } from '@/actions/post'
 
 interface AppGroupedPostsPageProps {
   title: string
@@ -9,33 +7,18 @@ interface AppGroupedPostsPageProps {
     title: string
     hiddenTags: string[]
   }[]
+  limit?: number
 }
 
 export default async function AppGroupedPostsPage({
   title,
   categories,
+  limit = 4,
 }: AppGroupedPostsPageProps) {
-  const categoriesData = []
-  for (const category of categories) {
-    const { title, hiddenTags } = category
-    categoriesData.push({
-      title,
-      posts: (await fetchPostsByHiddenTags(hiddenTags)).slice(0, 4),
-    })
-  }
-
   return (
     <AppPage>
       <h1 className="sr-only">{title}</h1>
-      <ul className="space-y-4">
-        {categoriesData.map(({ title, posts }, index) => (
-          <li key={index} className="space-y-4">
-            <h2 className="text-2xl uppercase text-center">{title}</h2>
-            <AppPostGrid posts={posts} />
-            {index !== categoriesData.length - 1 && <AppSeparator />}
-          </li>
-        ))}
-      </ul>
+      <AppMultiplePostGrids limit={limit} postGroups={categories} />
     </AppPage>
   )
 }
