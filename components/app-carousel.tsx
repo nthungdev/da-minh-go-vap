4681c371b1cce18bb65d3fdad5aff14ca42e425a
Hung Nguyen <nthungdev@gmail.com>
2024-09-23 03:14:26 -0400

    update carousel nav button color

--- components/app-carousel.tsx
+++ components/app-carousel.tsx
@@ -15,7 +15,7 @@ export default function AppCarousel(props: AppCarouselProps) {
   const hasMultipleChildren = childrenCount > 1
 
   return (
-    <div data-hs-carousel={JSON.stringify(hsCarouselData)} className="relative h-full">
+    <div data-hs-carousel={JSON.stringify(hsCarouselData)} className="relative h-full w-full">
       <div className="hs-carousel relative overflow-hidden w-full h-full bg-white">
         <div className="hs-carousel-body absolute top-0 bottom-0 start-0 flex flex-nowrap transition-transform duration-700 opacity-0">
           {Children.map(children, (child, index) => (
@@ -30,7 +30,7 @@ export default function AppCarousel(props: AppCarouselProps) {
         <Fragment>
           <button
             type="button"
-            className="hs-carousel-prev hs-carousel:disabled:opacity-50 disabled:pointer-events-none absolute inset-y-0 start-0 inline-flex justify-center items-center w-[46px] h-full text-gray-800 hover:bg-gray-800/10 focus:outline-none focus:bg-gray-800/10 hover:text-white focus:text-white rounded-s-lg"
+            className="hs-carousel-prev hs-carousel:disabled:opacity-50 disabled:pointer-events-none absolute inset-y-0 start-0 inline-flex justify-center items-center w-[46px] h-full text-gray-800 hover:bg-gray-800/10 focus:outline-none focus:bg-gray-800/10 hover:text-white rounded-s-lg"
           >
             <span className="text-2xl" aria-hidden="true">
               <svg
@@ -52,7 +52,7 @@ export default function AppCarousel(props: AppCarouselProps) {
           </button>
           <button
             type="button"
-            className="hs-carousel-next hs-carousel:disabled:opacity-50 disabled:pointer-events-none absolute inset-y-0 end-0 inline-flex justify-center items-center w-[46px] h-full text-gray-800 hover:bg-gray-800/10 focus:outline-none focus:bg-gray-800/10 hover:text-white focus:text-white rounded-e-lg"
+            className="hs-carousel-next hs-carousel:disabled:opacity-50 disabled:pointer-events-none absolute inset-y-0 end-0 inline-flex justify-center items-center w-[46px] h-full text-gray-800 hover:bg-gray-800/10 focus:outline-none focus:bg-gray-800/10 hover:text-white rounded-e-lg"
           >
             <span className="sr-only">Next</span>
             <span className="text-2xl" aria-hidden="true">
