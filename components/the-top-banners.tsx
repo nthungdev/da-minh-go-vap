4dabb5813a151d3e8d55c8ec3dfab313ae0fd4b8
Hung Nguyen <31390562+nthungdev@users.noreply.github.com>
2024-09-23 01:45:23 -0400

    sticky navbar (#114)

---
+++ components/the-top-banners.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { usePathname } from 'next/navigation'
+import { attributes as navbarAttributes } from '@/content/settings/navbar.md'
+import AppBanners from './app-banners'
+
+interface TheTopBannersProps {
+  className?: string
+}
+
+export default function TheTopBanners({ className }: TheTopBannersProps) {
+  const { pageBanners } = navbarAttributes as NavbarAttributes
+
+  const pathname = usePathname()
+  const banners = pageBanners.find((banner) => pathname == banner.path)?.banners
+  const hasBanners = !!banners
+
+  return (
+    <div className={`w-full ${className || ''}`}>
+      {hasBanners && <AppBanners banners={banners} />}
+    </div>
+  )
+}
