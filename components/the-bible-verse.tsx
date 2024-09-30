206dd3e2aeb25d8c18aff96a3d5c317a2f58dfa8
Hung Nguyen <nthungdev@gmail.com>
2024-09-30 18:16:32 -0400

    remove interval for single verse

--- components/the-bible-verse.tsx
+++ components/the-bible-verse.tsx
@@ -17,6 +17,8 @@ export default function TheBibleVerse(props: TheBibleVerseProps) {
   const [currentIndex, setCurrentIndex] = useState(0)
 
   useEffect(() => {
+    if (verses.length === 0) return
+
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) =>
         prevIndex === verses.length - 1 ? 0 : prevIndex + 1
@@ -31,11 +33,12 @@ export default function TheBibleVerse(props: TheBibleVerseProps) {
   const reference = currentVerse.reference
 
   return (
-    <blockquote className="relative border-2 border-primary rounded-md flex items-center justify-center py-8 px-2 bg-white">
+    <blockquote className="relative border-2 border-primary rounded-md flex flex-col items-center justify-center py-8 px-4 sm:px-10 bg-white">
       <QuoteOpen className="absolute top-0 start-0 size-8 text-primary" />
       <QuoteClose className="absolute bottom-0 end-0 size-8 text-primary" />
 
       <p className="text-center">{verse}</p>
+      <cite className="block w-full text-right">{reference}</cite>
     </blockquote>
   )
 }
