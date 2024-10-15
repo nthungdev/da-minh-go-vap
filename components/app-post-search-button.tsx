610fe9772c0ae71ba7d89bad1f5b6ad0b742f06e
Hung Nguyen <nthungdev@gmail.com>
2024-10-15 03:39:41 -0400

    update theme

--- components/app-post-search-button.tsx
+++ components/app-post-search-button.tsx
@@ -75,7 +75,7 @@ const SearchModal = forwardRef<HTMLDivElement, SearchModalProps>(
                     </div>
                     <input
                       id={`${id}-input`}
-                      className="py-3 ps-10 pe-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
+                      className="py-3 ps-10 pe-4 block w-full border-gray-200 rounded-lg text-sm focus:border-secondary          focus:ring-secondary disabled:opacity-50 disabled:pointer-events-none"
                       type="text"
                       role="search"
                       value={input}
