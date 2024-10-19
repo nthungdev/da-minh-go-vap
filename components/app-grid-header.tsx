a8597a925a7cf460a44030b97c89e9998c7f0e13
Hung Nguyen <nthungdev@gmail.com>
2024-10-18 20:23:33 -0400

    update header font

--- components/app-grid-header.tsx
+++ components/app-grid-header.tsx
@@ -6,7 +6,7 @@ export default function AppGridHeader({
   return (
     <div className="flex flex-row text-center text-primary-800 items-center">
       <div className="flex-1 h-[2px] bg-primary-800"></div>
-      <span className="mx-1.5 uppercase font-bold text-2xl">{text}</span>
+      <span className="mx-1.5 uppercase font-bold text-2xl font-header">{text}</span>
       <div className="flex-1 h-[2px] bg-primary-800"></div>
     </div>
   )
