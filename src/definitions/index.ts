10c59b71cbfa3a6c04453560f78eb427bd5fed38
Hung Nguyen <31390562+nthungdev@users.noreply.github.com>
2024-10-31 20:45:16 -0400

    Links in aside section (#173)
    * update social links design
    
    * rename field and add ring
    
    * update ring

--- src/definitions/index.ts
+++ src/definitions/index.ts
@@ -318,13 +318,12 @@ type AsideSection = {
     /** post slugs */
     posts: string[]
   }
-  socialLinks: {
+  links: {
     enable: boolean
     title: string
     links: {
-      type: string
-      name: string
       url: string
+      image: Image
     }[]
   }
 }
