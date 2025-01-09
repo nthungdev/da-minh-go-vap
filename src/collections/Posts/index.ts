7bad10295eeda0ee43a8751892fc934189c178cf
Hung Nguyen <nthungdev@gmail.com>
2025-01-08 20:09:53 -0500

    posts collection

---
+++ src/collections/Posts/index.ts
@@ -0,0 +1,67 @@
+import markdownField from '@/fields/markdown'
+import type { CollectionConfig } from 'payload'
+
+const Posts: CollectionConfig = {
+  slug: 'posts',
+  access: {
+    read: () => true,
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      label: 'Tiêu đề',
+      required: true,
+    },
+    {
+      name: 'publishedAt',
+      type: 'date',
+      label: 'Thời gian công bố',
+      required: true,
+      admin: { position: 'sidebar' },
+    },
+    {
+      name: 'hiddenTags',
+      type: 'relationship',
+      relationTo: 'hiddenTags',
+      hasMany: true,
+    },
+    {
+      name: 'thumbnail',
+      type: 'upload',
+      relationTo: 'media',
+    },
+    {
+      name: 'videos',
+      type: 'array',
+      fields: [
+        {
+          name: 'title',
+          type: 'text',
+          label: 'Tiêu đề',
+          required: true,
+        },
+        {
+          name: 'type',
+          type: 'select',
+          options: [
+            { label: 'Youtube', value: 'youtube' },
+            { label: 'Facebook', value: 'facebook' },
+          ],
+        },
+        {
+          name: 'url',
+          type: 'text',
+          label: 'URL',
+          required: true,
+        },
+      ],
+    },
+    {
+      ...markdownField('body'),
+      required: true,
+    },
+  ],
+}
+
+export default Posts
