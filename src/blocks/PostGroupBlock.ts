851dba56b42fec4cb139a72a7258a752d2cd708c
Hung Nguyen <nthungdev@gmail.com>
2025-02-13 20:34:27 -0500

    add PostGroupBlock

---
+++ src/blocks/PostGroupBlock.ts
@@ -0,0 +1,45 @@
+import markdownField from '@/fields/markdown'
+import { Block } from 'payload'
+
+const PostGroupBlock: Block = {
+  slug: 'postGroupBlock',
+  labels: {
+    singular: 'Post Group Block',
+    plural: 'Post Group Blocks',
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'hiddenTags',
+      type: 'relationship',
+      relationTo: 'hiddenTags',
+      hasMany: true,
+      required: true,
+      unique: true,
+    },
+    {
+      name: 'viewMoreButton',
+      type: 'group',
+      fields: [
+        {
+          name: 'enableViewMoreButton',
+          type: 'checkbox',
+          defaultValue: false,
+          required: true,
+        },
+        {
+          name: 'relativeUrl',
+          type: 'relationship',
+          relationTo: 'posts',
+          hasMany: false,
+        },
+      ],
+    },
+  ],
+}
+
+export default PostGroupBlock
