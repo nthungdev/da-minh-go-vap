b7655f7c00e3dd1bdd2ed71936dfd205274f366a
Hung Nguyen <nthungdev@gmail.com>
2025-02-23 14:02:35 -0500

    deprecate pages

---
+++ src/app/(app)/[[...path]]/page.tsx
@@ -0,0 +1,41 @@
+import { getPayload } from 'payload'
+import config from '@payload-config'
+import AppPage from '@/components/app-page'
+
+// export const dynamicParams = true // or false, to 404 on unknown paths
+export const dynamic = 'force-static'
+
+export async function generateStaticParams() {
+  const payload = await getPayload({ config })
+  const pages = await payload.find({ collection: 'pages' })
+  return pages.docs.map((page) => ({
+    path: page.path.split('/'),
+  }))
+}
+
+export default async function Page(props: {
+  params: Promise<{ path: string[] }>
+}) {
+  const params = await props.params
+  console.log({ params })
+  const path = '/' + params.path?.join('/') || ''
+  const payload = await getPayload({ config })
+  const query = await payload.find({
+    collection: 'pages',
+    where: {
+      path: { equals: path },
+    },
+  })
+  const page = query.docs[0]
+  if (!page) {
+    console.log('page not found')
+    // TODO
+  }
+
+  console.log({ page })
+  return (
+    <AppPage>
+      <h1 className="sr-only">{page.title}</h1>
+    </AppPage>
+  )
+}
