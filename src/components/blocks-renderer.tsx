0e6708c9f3042d9ccb309ad0fb80de74276c9bd4
Hung Nguyen <nthungdev@gmail.com>
2025-02-28 16:06:50 -0500

    block renderer to handle tabbedPostGroupBlock and spaceBlock

--- src/components/blocks-renderer.tsx
+++ src/components/blocks-renderer.tsx
@@ -1,29 +1,51 @@
+import AppSpace from '@/components/app-space'
+import AppTabbedPostGroup from '@/components/app-tabbed-post-group'
 import TheBibleVerse from '@/components/the-bible-verse'
+import { BlockType } from '@/definitions'
 
-const componentsMap = {
+const componentsMap: {
+  [key in BlockType['blockType']]?: React.ComponentType<any>
+} = {
   bibleVerseBlock: TheBibleVerse,
+  tabbedPostGroupBlock: AppTabbedPostGroup,
+  spaceBlock: AppSpace,
 }
 
-function mapComponentProps(block: any) {
-  if (block.blockType === 'bibleVerseBlock') {
-    return {
-      verses: [
-        {
-          verse: block.verse as string,
-          reference: block.reference as string,
-        },
-      ],
-    }
-  } else {
-    // TODO
-    return null
+function mapComponentProps(block: BlockType) {
+  switch (block.blockType) {
+    case 'bibleVerseBlock':
+      return {
+        verses: [
+          {
+            verse: block.verse as string,
+            reference: block.reference as string,
+          },
+        ],
+      }
+
+    case 'tabbedPostGroupBlock':
+      return {
+        title: block.title,
+        groups: (block.tabs as any[]).map((tab) => ({
+          title: tab.title,
+          hiddenTags: tab.hiddenTags.map((t: any) => t.tag),
+        })),
+      }
+
+    case 'spaceBlock':
+      return {
+        size: block.size,
+      }
+
+    default:
+      return null
   }
 }
 
-const BlocksRenderer = ({ blocks }: { blocks: any[] }) => {
+const BlocksRenderer = ({ blocks }: { blocks: BlockType[] }) => {
   return (
     <div>
-      {blocks.map((block: { blockType: keyof typeof componentsMap }, index) => {
+      {blocks.map((block, index) => {
         const Component = componentsMap[block.blockType]
         if (!Component) return null
         return <Component key={index} {...mapComponentProps(block)!} />
