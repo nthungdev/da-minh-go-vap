4882a7b03e02534877151626a8806f98ee95975a
Hung Nguyen <nthungdev@gmail.com>
2025-03-07 04:40:21 -0500

    update AppTimelineCards

--- src/components/app-timeline-cards.tsx
+++ src/components/app-timeline-cards.tsx
@@ -1,40 +1,52 @@
-import Image from "next/image";
-import Link from "next/link";
+import AppSectionHeader from '@/components/app-section-header'
+import { Media } from '@/payload-types'
+import classNames from 'classnames'
+import Image from 'next/image'
+import Link from 'next/link'
 
 interface AppTimelineCardsProps {
-  className?: string;
+  className?: string
   cards: {
-    title: string;
-    thumbnail: {
-      url: string;
-      alt: string;
-    };
-    url: string;
-  }[];
+    title: string
+    thumbnail: Media
+    url: string
+  }[]
+  title: string
 }
 
-export default function AppTimelineCards({ className, cards }: AppTimelineCardsProps) {
-
+export default function AppTimelineCards({
+  className,
+  cards,
+  title,
+}: AppTimelineCardsProps) {
   return (
     <div className={`space-y-12 ${className}`}>
+      {title && (
+        <AppSectionHeader className="text-2xl">{title}</AppSectionHeader>
+      )}
+
       <div className="flex flex-col">
         {cards.map((card, index) => (
           <div
             key={index}
-            className={`flex ${index % 2 === 0 ? 'flex-row' : 'md:flex-row-reverse'
-              }`}
+            className={`flex ${
+              index % 2 === 0 ? 'flex-row' : 'md:flex-row-reverse'
+            }`}
           >
             <Link
-              href={card.url}
+              href={card.url || '#'}
               className={`my-2 flex-1 space-y-2 hover:ring-2 rounded-lg order-last md:order-none`}
             >
               <div className="relative aspect-video w-full">
-                <Image
-                  src={card.thumbnail.url}
-                  alt={card.thumbnail.alt}
-                  className="w-full rounded-lg object-cover"
-                  fill
-                />
+                {typeof card.thumbnail !== 'string' &&
+                  typeof card.thumbnail.url === 'string' && (
+                    <Image
+                      src={card.thumbnail.url}
+                      alt={card.thumbnail.alt}
+                      className="w-full rounded-lg object-cover"
+                      fill
+                    />
+                  )}
               </div>
               <h2 className="text-xl font-semibold px-2 pb-1 text-center">
                 {card.title}
@@ -43,17 +55,22 @@ export default function AppTimelineCards({ className, cards }: AppTimelineCardsP
 
             <div className="relative px-2">
               <div
-                className={`absolute left-0 right-0 top-0 h-full w-1 bg-primary mx-auto
-            ${index === 0 ? 'rounded-t-full' : ''}
-            ${index === cards.length - 1 ? 'rounded-b-full' : ''}`}
+                className={classNames(
+                  `absolute left-0 right-0 top-0 h-full w-1 bg-primary mx-auto`,
+                  index === 0 ? 'rounded-t-full' : '',
+                  index === cards.length - 1 ? 'rounded-b-full' : ''
+                )}
               ></div>
               <div className="relative mt-2 bg-red-300 rounded-full w-8 h-8">
-                <Image
-                  src={card.thumbnail.url}
-                  alt={card.thumbnail.alt}
-                  className="w-full rounded-full object-cover"
-                  fill
-                />
+                {typeof card.thumbnail !== 'string' &&
+                  typeof card.thumbnail.url === 'string' && (
+                    <Image
+                      src={card.thumbnail.url}
+                      alt={card.thumbnail.alt}
+                      className="w-full rounded-lg object-cover"
+                      fill
+                    />
+                  )}
               </div>
             </div>
 
