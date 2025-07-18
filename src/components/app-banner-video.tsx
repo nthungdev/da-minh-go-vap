a599de5eb1addff9436bcfba41ed4b5338328268
Hung Nguyen <nthungdev@gmail.com>
2025-07-17 20:46:13 -0400

    fix eslint not working

--- src/components/app-banner-video.tsx
+++ src/components/app-banner-video.tsx
@@ -6,12 +6,12 @@ interface AppBannerVideoProps extends VideoHTMLAttributes<HTMLVideoElement> {}
 export default function AppBannerVideo(props: AppBannerVideoProps) {
   return (
     <video
-      {...props}
       className={twMerge("object-cover h-full w-full", props.className)}
       autoPlay
       loop
       muted
       playsInline
+      {...props}
     />
   );
 }
