4ac722028bd565213411a3eecbafd0b184273dd3
Hung Nguyen <nthungdev@gmail.com>
2025-07-19 20:27:51 -0400

    update footer

--- src/components/the-footer.tsx
+++ src/components/the-footer.tsx
@@ -22,7 +22,7 @@ export default async function TheFooter() {
   );
 
   return (
-    <footer className="relative">
+    <footer className="relative bg-primary">
       {footer.topFooterDecorativeGraphic &&
         typeof footer.topFooterDecorativeGraphic !== "string" &&
         typeof footer.topFooterDecorativeGraphic.url === "string" && (
@@ -62,7 +62,7 @@ export default async function TheFooter() {
       </div>
 
       {/* layout for large screens */}
-      <div className="hidden sm:flex flex-col md:flex-row left-0 bottom-0 px-4 py-4 max-w-screen-xl gap-8 bg-primary text-white text-left text-[0.9vw] 3xl:text-xl">
+      <div className="hidden sm:flex flex-col md:flex-row left-0 bottom-0 px-4 py-4 mx-auto max-w-screen-xl gap-8 text-white text-left text-[0.9vw] 3xl:text-xl">
         <Contact
           name={footer.contact.name}
           phone={footer.contact.phone}
