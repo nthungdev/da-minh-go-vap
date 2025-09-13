a9412c9c32813320a169cee613542c7e03f89b57
Hung Nguyen <nthungdev@gmail.com>
2025-09-13 17:28:58 -0400

    refactor: improve data validation in getDataOrUndefined function

--- src/payload/utils/data.ts
+++ src/payload/utils/data.ts
@@ -1,10 +1,18 @@
+export function isDataValid<T extends object>(
+  data: string | T | null | undefined,
+) {
+  return !!data && typeof data !== "string";
+}
+
 /**
  * If the data is not an object, it's considered not existed.
  * This utility function is used to ensure that we only return valid data objects.
  * @param data
  */
-export function getDataOrUndefined<T>(data: string | T | null | undefined) {
-  return !data || typeof data === "string" ? undefined : data;
+export function getDataOrUndefined<T extends object>(
+  data: string | T | null | undefined,
+) {
+  return isDataValid(data) ? data : null;
 }
 
 export function slugify(str: string): string {
