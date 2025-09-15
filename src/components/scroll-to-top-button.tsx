0cf13530060b8d765e9d30fbf4d58caf4e046ed2
Hung Nguyen <31390562+nthungdev@users.noreply.github.com>
2025-09-15 02:37:34 -0400

    add scroll to top botton (#224)

---
+++ src/components/scroll-to-top-button.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { FaArrowUp } from "react-icons/fa";
+
+export default function ScrollToTopButton() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  // Show button when page is scrolled down
+  useEffect(() => {
+    const toggleVisibility = () => {
+      console.log({
+        scrollY: window.scrollY,
+      });
+      if (window.scrollY > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  if (!isVisible) return null;
+
+  return (
+    <button
+      onClick={scrollToTop}
+      className="text-primary-700 hover:bg-primary-700 fixed right-6 bottom-6 z-20 rounded-full bg-gray-200/50 p-3 shadow-lg transition-all hover:text-white"
+    >
+      <FaArrowUp className="size-4" />
+    </button>
+  );
+}
