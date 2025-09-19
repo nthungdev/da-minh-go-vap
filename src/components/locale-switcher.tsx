eddca1c4dc3935947995b62a6aba5d48085dcf89
Hung Nguyen <nthungdev@gmail.com>
2025-09-19 04:20:47 -0400

    add loading state to locale switcher display locale switcher

--- src/components/locale-switcher.tsx
+++ src/components/locale-switcher.tsx
@@ -1,7 +1,11 @@
+"use client";
+
 import { setLocale } from "@/actions/locale";
+import Spinner from "@/components/spinner";
 import { Config } from "@/payload-types";
 import { defaultLocale } from "@/utils/constants";
 import { useEffect, useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 const localeLabels: Record<Config["locale"], string> = {
   en: "English",
@@ -13,6 +17,7 @@ export default function LocaleSwitcher({
 }: {
   initialLocale?: Config["locale"];
 }) {
+  const [loading, setLoading] = useState(false);
   const [locale, sLocale] = useState<Config["locale"]>(
     initialLocale || defaultLocale,
   );
@@ -23,16 +28,27 @@ export default function LocaleSwitcher({
   }, []);
 
   async function switchLocale() {
+    setLoading(true);
     await setLocale(locale === "en" ? "vi" : "en");
     window.location.reload();
   }
 
   return (
     <button
-      className="group rounded px-2 py-1 hover:cursor-pointer"
+      className="group relative rounded px-2 py-1 hover:cursor-pointer"
       onClick={switchLocale}
     >
-      {label}
+      <span className={loading ? "opacity-30" : ""}>{label}</span>
+      <div
+        className={twMerge(
+          "absolute top-0 right-0 bottom-0 left-0 flex size-full items-center justify-center",
+          !loading && "hidden",
+        )}
+      >
+        <Spinner
+          className={twMerge("size-4 text-white", loading ? "block" : "hidden")}
+        />
+      </div>
     </button>
   );
 }
