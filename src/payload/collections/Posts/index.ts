2b57df6df76ef3202320c75e832c960762a74ee0
Hung Nguyen <nthungdev@gmail.com>
2025-09-20 19:16:39 -0400

    set localized for Posts

--- src/payload/collections/Posts/index.ts
+++ src/payload/collections/Posts/index.ts
@@ -37,6 +37,7 @@ const Posts: CollectionConfig = {
       hooks: {
         beforeDuplicate: [duplicateTitle],
       },
+      localized: true,
     },
     {
       name: "slug",
@@ -92,6 +93,7 @@ const Posts: CollectionConfig = {
           type: "text",
           label: "Tiêu đề",
           required: true,
+          localized: true,
         },
         {
           name: "type",
@@ -117,6 +119,7 @@ const Posts: CollectionConfig = {
     {
       ...markdownField("body"),
       required: true,
+      localized: true,
     },
   ],
   hooks: {
