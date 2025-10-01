1d9de1fd585c25ff263401da630da91b5f7a5b1e
Hung Nguyen <nthungdev@gmail.com>
2025-10-01 09:40:12 -0400

    change default locale to vi

--- src/i18n/config.ts
+++ src/i18n/config.ts
@@ -1,7 +1,7 @@
 export type Locale = (typeof locales)[number];
 
 export const locales = ["en", "vi"] as const;
-export const defaultLocale: Locale = "en";
+export const defaultLocale: Locale = "vi";
 
 export const localeLabels: Record<Locale, string> = {
   en: "English",
