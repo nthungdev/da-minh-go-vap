62747cdb1bde6217171738f6b16bacab1922164b
Hung Nguyen <nthungdev@gmail.com>
2025-10-04 00:19:13 -0400

    back up tabbed post group

---
+++ src/components/app-tabbed-post-group-old.tsx
@@ -0,0 +1,32 @@
+import AppGridHeader from "@/components/app-grid-header";
+import AppPostGridSix from "@/components/app-post-grid-five";
+import AppPostTabGridAsync from "@/components/app-post-tab-grid-async";
+import { Page } from "@/payload-types";
+
+interface AppTabbedPostGroupProps {
+  title?: string;
+  groups: {
+    title: string;
+    hiddenTags: string[];
+    limit: number;
+    viewMoreButton?: {
+      enable: boolean;
+      relativeUrl: Page | string | undefined | null;
+    };
+  }[];
+}
+
+async function AppTabbedPostGroup(props: AppTabbedPostGroupProps) {
+  return (
+    <div className="space-y-4">
+      {props.title && <AppGridHeader text={props.title} />}
+      <AppPostTabGridAsync
+        id={props.title?.replace(/\s+/g, "-").toLowerCase()}
+        postGroups={props.groups}
+        component={AppPostGridSix}
+      />
+    </div>
+  );
+}
+
+export default AppTabbedPostGroup;
