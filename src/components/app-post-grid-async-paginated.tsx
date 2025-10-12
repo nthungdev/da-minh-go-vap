67e00f33acad398d2de5f1bcb01b4eeab02228b2
Hung Nguyen <nthungdev@gmail.com>
2025-10-12 05:56:50 -0400

    implement pagination panel

--- src/components/app-post-grid-async-paginated.tsx
+++ src/components/app-post-grid-async-paginated.tsx
@@ -2,13 +2,17 @@
 
 import { fetchPostsByHiddenTags } from "@/actions/post";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
-import Image from "next/image";
-import Link from "next/link";
-import { useState } from "react";
+import { HTMLAttributes, useState } from "react";
 import AppPostGridSkeleton from "./app-post-grid-skeleton";
 import { AppPost } from "@/definitions";
 import { useLocale } from "next-intl";
 import AppPostGrid from "@/components/app-post-grid";
+import { IoIosSkipBackward, IoIosSkipForward } from "react-icons/io";
+import {
+  MdOutlineArrowBackIosNew,
+  MdOutlineArrowForwardIos,
+} from "react-icons/md";
+import { twMerge } from "tailwind-merge";
 
 const DEFAULT_PAGE_SIZE = 12;
 
@@ -23,7 +27,6 @@ interface AppPostGridPaginatedProps {
 export default function AppPostGridPaginated({
   hiddenTags,
   pageSize = DEFAULT_PAGE_SIZE,
-  className,
   skipSlug,
   posts: initialPosts,
 }: AppPostGridPaginatedProps) {
@@ -48,21 +51,10 @@ export default function AppPostGridPaginated({
     },
   });
 
-  function incrementPage() {
-    setPage((prevPage) => prevPage + 1);
-  }
-
-  function decrementPage() {
-    setPage((prevPage) => Math.max(prevPage - 1, 0));
-  }
-
   if (!data) return undefined;
 
   const { posts, hasMore } = data;
   const hidePagination = !hasMore && page === 1;
-
-  const disablePreviousPage = isFetching || page === 1;
-  const disableNextPage = isFetching || !hasMore;
 
   return (
     <div className="space-y-2">
@@ -75,24 +67,76 @@ export default function AppPostGridPaginated({
       )}
 
       {!hidePagination && (
-        <div className="flex flex-row justify-end">
-          <button
-            className="bg-primary-50 text-primary-700 disabled:bg-primary-50 disabled:text-primary-700 hover:bg-primary-50 hover:text-primary-600 rounded-l-lg border px-3 py-1 hover:cursor-pointer disabled:cursor-not-allowed disabled:opacity-50"
-            disabled={disablePreviousPage}
-            onClick={decrementPage}
-          >
-            Quay lại
-          </button>
-
-          <button
-            className="bg-primary-1 text-primary-700 disabled:bg-primary-50 disabled:text-primary-700 hover:bg-primary-50 hover:text-primary-600 rounded-r-lg border px-3 py-1 hover:cursor-pointer disabled:cursor-not-allowed disabled:opacity-50"
-            disabled={disableNextPage}
-            onClick={incrementPage}
-          >
-            Xem tiếp
-          </button>
-        </div>
+        <PaginationPanel
+          className="mt-4 md:mt-8"
+          totalPages={data.totalPages}
+          page={data.page}
+          onPageChange={setPage}
+        />
       )}
+    </div>
+  );
+}
+
+interface PaginationPanel extends HTMLAttributes<HTMLDivElement> {
+  totalPages: number;
+  page: number;
+  onPageChange?: (page: number) => void;
+}
+
+function PaginationPanel({
+  totalPages,
+  page,
+  className,
+  onPageChange,
+}: PaginationPanel) {
+  const allPages = Array(totalPages)
+    .fill(null)
+    .map((_, index) => index + 1);
+  const shownPages = allPages.slice(Math.max(page - 3, 0), page + 2);
+
+  return (
+    <div className={twMerge("flex flex-row justify-center gap-x-1", className)}>
+      <button
+        className="text-primary rounded-md border px-3 py-1 hover:cursor-pointer disabled:text-black disabled:hover:cursor-auto"
+        onClick={() => onPageChange?.(page - 1)}
+        disabled={page <= 1}
+      >
+        <MdOutlineArrowBackIosNew />
+      </button>
+      <button
+        className="text-primary rounded-md border px-3 py-1 hover:cursor-pointer disabled:text-black disabled:hover:cursor-auto"
+        disabled={page <= 1}
+        onClick={() => onPageChange?.(1)}
+      >
+        <IoIosSkipBackward />
+      </button>
+      {shownPages.map((p) => (
+        <button
+          key={p}
+          className={
+            "text-primary rounded-md border px-3 py-1 hover:cursor-pointer disabled:text-black disabled:hover:cursor-auto"
+          }
+          disabled={p === page}
+          onClick={() => onPageChange?.(p)}
+        >
+          {p}
+        </button>
+      ))}
+      <button
+        className="text-primary rounded-md border px-3 py-1 hover:cursor-pointer disabled:text-black disabled:hover:cursor-auto"
+        disabled={page >= totalPages}
+        onClick={() => onPageChange?.(totalPages)}
+      >
+        <IoIosSkipForward />
+      </button>
+      <button
+        className="text-primary rounded-md border px-3 py-1 hover:cursor-pointer disabled:text-black disabled:hover:cursor-auto"
+        onClick={() => onPageChange?.(page + 1)}
+        disabled={page >= totalPages}
+      >
+        <MdOutlineArrowForwardIos />
+      </button>
     </div>
   );
 }
