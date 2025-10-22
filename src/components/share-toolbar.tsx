fccadab329a005534c8d843fc23bcc9ea23ef8ab
Hung Nguyen <nthungdev@gmail.com>
2025-10-22 05:40:25 -0400

    feat: display title share toolbar

--- src/components/share-toolbar.tsx
+++ src/components/share-toolbar.tsx
@@ -1,4 +1,5 @@
 import CopyButton from "@/components/copy-button";
+import { getMessages } from "next-intl/server";
 import Link from "next/link";
 import { HTMLAttributes } from "react";
 import { FaViber } from "react-icons/fa";
@@ -16,32 +17,37 @@ interface ShareToolbarProps extends HTMLAttributes<HTMLDivElement> {
   shareUrl: string;
 }
 
-export default function ShareToolbar({
+export default async function ShareToolbar({
   shareUrl,
   className,
 }: ShareToolbarProps) {
   const viberHref = `viber://forward?text=${shareUrl}`;
   const facebookHref = buildFacebookShareUrl(shareUrl);
 
+  const messages = await getMessages();
+
   return (
-    <div className={twMerge("flex flex-row gap-2", className)}>
-      <CopyButton
-        text={shareUrl}
-        className="hover:border-primary cursor-pointer rounded-full border p-2"
-      />
-      <Link
-        href={facebookHref}
-        target="_blank"
-        className="hover:border-primary cursor-pointer rounded-full border p-2 text-[#1877F2]"
-      >
-        <RiFacebookFill />
-      </Link>
-      <Link
-        href={viberHref}
-        className="hover:border-primary rounded-full border p-2 text-[#7360f2]"
-      >
-        <FaViber />
-      </Link>
+    <div className={className}>
+      <div>{messages.shareToolbar.title}</div>
+      <div className={twMerge("mt-1 flex flex-row gap-2")}>
+        <CopyButton
+          text={shareUrl}
+          className="hover:border-primary cursor-pointer rounded-full border p-2"
+        />
+        <Link
+          href={facebookHref}
+          target="_blank"
+          className="hover:border-primary cursor-pointer rounded-full border p-2 text-[#1877F2]"
+        >
+          <RiFacebookFill />
+        </Link>
+        <Link
+          href={viberHref}
+          className="hover:border-primary rounded-full border p-2 text-[#7360f2]"
+        >
+          <FaViber />
+        </Link>
+      </div>
     </div>
   );
 }
