2e764d793d87f9c94a5c4062b0e79386c76e4355
Hung Nguyen <nthungdev@gmail.com>
2026-03-02 16:11:17 -0500

    feat: integrate cloudflare r2 via s3 plugin

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -1,5 +1,9 @@
 import { default as default_8cbc64bd7b83ad69a9631721ac98586c } from '@/payload/fields/markdown/Field'
+import { S3ClientUploadHandler as S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24 } from '@payloadcms/storage-s3/client'
+import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
 
 export const importMap = {
-  "@/payload/fields/markdown/Field#default": default_8cbc64bd7b83ad69a9631721ac98586c
+  "@/payload/fields/markdown/Field#default": default_8cbc64bd7b83ad69a9631721ac98586c,
+  "@payloadcms/storage-s3/client#S3ClientUploadHandler": S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24,
+  "@payloadcms/next/rsc#CollectionCards": CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1
 }
