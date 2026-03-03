5a348f80cc3bc6249afe1708ee5d3a07de311651
Hung Nguyen <nthungdev@gmail.com>
2026-03-03 08:42:57 -0500

    feat: show placeholder for app post grid

--- src/components/app-post-grid-skeleton.tsx
+++ src/components/app-post-grid-skeleton.tsx
@@ -1,19 +1,22 @@
 export default function AppPostGridSkeleton({ count }: { count: number }) {
   return (
-    <ul className="relative grid grid-flow-row gap-4 md:grid-cols-2 lg:grid-cols-4">
-      {[...Array(count)].map((_, index) => (
-        <li
-          className="block w-full min-w-0 animate-pulse border border-transparent bg-white"
-          key={index}
-        >
-          <div className="block overflow-hidden border">
-            <div className="relative aspect-video bg-gray-200"></div>
-            <div className="space-y-2 p-2">
-              <div className="block h-7 truncate bg-gray-200 text-center text-xl"></div>
+    <div>
+      <div className="mb-4 block h-7 truncate bg-gray-200 text-center text-xl"></div>
+      <ul className="relative grid grid-flow-row gap-4 md:grid-cols-2 lg:grid-cols-4">
+        {[...Array(count)].map((_, index) => (
+          <li
+            className="block w-full min-w-0 animate-pulse border border-transparent bg-white"
+            key={index}
+          >
+            <div className="block overflow-hidden border">
+              <div className="relative aspect-video bg-gray-200"></div>
+              <div className="space-y-2 p-2">
+                <div className="block h-7 truncate bg-gray-200 text-center text-xl"></div>
+              </div>
             </div>
-          </div>
-        </li>
-      ))}
-    </ul>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
