b706c9427fd369c7a230b3be74fa36eaaad3c0b5
Hung Nguyen <nthungdev@gmail.com>
2026-03-18 20:37:16 -0400

    fix: padding of post list items

--- src/components/post-list.tsx
+++ src/components/post-list.tsx
@@ -20,7 +20,7 @@ export default function PostList({
   showShortBody,
 }: PostListProps) {
   return (
-    <ul className={twMerge("post-list space-y-2", className)}>
+    <ul className={twMerge("post-list", className)}>
       {posts.map((post, index) => {
         const thumbnail = getDataOrUndefined(post.thumbnail);
         const date = post.publishedAt.toLocaleDateString("vi-VN");
@@ -30,7 +30,7 @@ export default function PostList({
           <li
             key={index}
             className={twMerge(
-              "border-b-2 border-gray-200 pb-2 last:border-0 hover:cursor-pointer",
+              "border-b-2 border-gray-200 pt-2 pb-2 first:pt-0 last:border-0 hover:cursor-pointer",
               index === activeIndex && "bg-gray-200",
               effectOnHover &&
                 "transition-transform ease-in hover:scale-x-[103%] lg:hover:scale-x-105",
