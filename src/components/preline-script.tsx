92e2cfe91bc5e0b1aacecb75a8a6e1b5a43a33a4
Hung Nguyen <nthungdev@gmail.com>
2026-03-23 15:33:39 -0400

    refactor

--- src/components/preline-script.tsx
+++ src/components/preline-script.tsx
@@ -2,25 +2,22 @@
 
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
-
 import { IStaticMethods } from "preline/preline";
+
 declare global {
   interface Window {
     HSStaticMethods: IStaticMethods;
   }
 }
 
+// Preline UI
 export default function PrelineScript() {
   const path = usePathname();
 
   useEffect(() => {
-    const loadPreline = async () => {
-      await import("preline/preline");
-
+    import("preline/preline").then(() => {
       window.HSStaticMethods.autoInit();
-    };
-
-    loadPreline();
+    });
   }, [path]);
 
   return null;
