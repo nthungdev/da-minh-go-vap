ae55233d2c56f744b6927df7d8036ad19dc156e3
Hung Nguyen <31390562+nthungdev@users.noreply.github.com>
2026-03-23 16:31:08 -0400

    276 fix fuses not indexing new posts (#297)
    * feat: search posts using locale
    
    * feat: update post fuse after updating posts collection

--- src/utils/fuses.ts
+++ src/utils/fuses.ts
@@ -1,4 +1,5 @@
 import { AppPost } from "@/definitions";
+import { Locale } from "@/i18n/config";
 import { postToAppPost } from "@/utils/post";
 import { queryAllPosts } from "@/utils/post-server";
 import Fuse from "fuse.js";
@@ -6,6 +7,7 @@ import Fuse from "fuse.js";
 class Fuses {
   static #instance: Fuses;
   private postFuse?: Fuse<AppPost>;
+  private postFuses: Record<Locale, Fuse<AppPost>> = Object();
 
   private constructor() {}
 
@@ -23,6 +25,28 @@ class Fuses {
       this.postFuse = new Fuse(posts, { keys: ["title"] });
     }
     return this.postFuse;
+  }
+
+  async getPostFuseByLocale(locale: Locale) {
+    if (!this.postFuses[locale]) {
+      const q = await queryAllPosts({ limit: 9999, locale });
+      const posts = q.docs.map(postToAppPost);
+      this.postFuses[locale] = new Fuse(posts, { keys: ["title"] });
+    }
+    return this.postFuses[locale];
+  }
+
+  async addPost(post: AppPost, locale: Locale) {
+    const fuse = this.postFuses[locale];
+    if (!fuse) return;
+    fuse.add(post);
+  }
+
+  async replacePost(post: AppPost, locale: Locale) {
+    const fuse = this.postFuses[locale];
+    if (!fuse) return;
+    fuse.remove((p) => p.id === post.id);
+    fuse.add(post);
   }
 }
 
