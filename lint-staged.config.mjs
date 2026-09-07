c50f46ce2da8c567fc89f3c063e20ee142631f89
Hung Nguyen <nthungdev@gmail.com>
2026-09-06 22:52:57 -0400

    chore: update CI setup, add type check to lint-staged, and standardize typecheck script

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,6 +1,7 @@
 const lintStagedConfig = {
   "*.{js,ts,jsx,tsx,json}": "eslint --cache --fix",
   "*.{js,ts,jsx,tsx,css,md,json,yaml,yml}": "prettier --write",
+  "*.{ts,tsx}": () => "tsc --noEmit",
 };
 
 export default lintStagedConfig;
